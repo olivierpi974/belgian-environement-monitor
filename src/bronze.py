97ccc300f353ed060e87e53795571fd9198b60f2
Olivier Picaud <picaud olivier>
2026-05-06 18:14:10 +0200

    feat(bronze): add load_bronze pipeline with monitoring logs

---
+++ src/bronze.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import time
+import os
+import datetime
+
+def load_bronze(path,engine):
+    date= datetime.datetime.now()
+    start=time.perf_counter()
+    #loading the dataframe from the csv file
+    table_name=os.path.splitext(os.path.basename(path))[0].lower()
+    print(f"reading {table_name}.csv...")
+    df=pd.read_csv(path,sep=',',low_memory=False)
+    print(f"{table_name}.csv loaded : {len(df)} lignes")
+
+    # sending the dataframe to the database
+    df.to_sql(table_name, con=engine,schema="bronze", if_exists="replace",index=False,chunksize=10000)
+
+    #coubting the ingestion time
+    elapsed= time.perf_counter()-start
+    print("loading done")
+    print(f"{table_name}|nombre de colonnes:{len(df.columns)}|{len(df)} lignes| {elapsed:.2f} s|date_ingest:{date}")
+
+    log_dict={'table':table_name,
+              "nb_lines":len(df),
+              "date_ingest":date,
+              "duration_sec":round(elapsed,2),
+              "nb_cols":len(df.columns)}
+    print("creating the monitoring table")
+    df_log=pd.DataFrame([log_dict])
+
+    df_log.to_sql("bronze_monitoring", con=engine,schema="monitoring", if_exists="append",index=False,chunksize=10000)
+    print("loading the monitoring table")
+    return log_dict
--- src/main.py
+++ src/main.py
@@ -1,3 +1,20 @@
 from utils.db import connect_to_db
+from bronze import load_bronze
 
-connect_to_db()
+#path to csv
+f1_path = "data/raw/F1_4_Air_Releases_Facilities.csv"
+f5_path = "data/raw/F5_2_LCP_Energy_Emissions.csv"
+f6_1_path="data/raw/F6_1_IED_Installations.csv"
+
+#instantication of engine
+engine=connect_to_db()
+
+#loading to bronze
+load_bronze(f1_path,engine)
+load_bronze(f5_path,engine)
+
+print("starting F6_1...")
+try:
+    load_bronze(f6_1_path, engine)
+except Exception as e:
+    print(f"Erreur F6_1 : {e}")
