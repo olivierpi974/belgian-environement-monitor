680aecc007d300616ad042d1f927fb28601ddbea
olivierpi974 <olivier.picaud@live.fr>
2026-05-17 15:36:58 +0200

    feat(sql):add gold DDL table to 03_create_table.sql '

--- sql/03_create_table.sql
+++ sql/03_create_table.sql
@@ -7,7 +7,8 @@ facilityName VARCHAR(100),
 city VARCHAR (50),
 Longitude NUMERIC(15,6),
 Latitude NUMERIC(15,6),
-emitted_co2_kg NUMERIC(15,4));
+pollutant VARCHAR(100),
+emitted_pollutant_kg NUMERIC(15,4));
 
 
 CREATE TABLE IF NOT EXISTS monitoring.silver_monitoring
@@ -44,4 +45,68 @@ installationinspireid VARCHAR(120),
 city_of_facility VARCHAR(50),
 longitude NUMERIC(15,6),
 latitude NUMERIC(15,6),
-PRIMARY KEY(installationinspireid,reportingyear))
+PRIMARY KEY(installationinspireid,reportingyear));
+
+CREATE TABLE IF NOT EXISTS silver.bridge_lcp_installation
+(lcpinspireid VARCHAR(120), 
+installationinspireid VARCHAR(120), 
+parent_facilityinspireid VARCHAR(120),
+lcp_name VARCHAR(255),
+installation_name VARCHAR(255),
+lcp_city VARCHAR(100),
+installation_city VARCHAR(100),
+lcp_longitude DOUBLE PRECISION,
+lcp_latitude DOUBLE PRECISION,
+installation_longitude DOUBLE PRECISION,
+installation_latitude DOUBLE PRECISION,
+distance_km DOUBLE PRECISION, 
+name_similarity DOUBLE PRECISION,
+same_city_flag BOOLEAN,
+id_guess_match BOOLEAN,
+same_city_search_used BOOLEAN,
+candidate_rank INT,
+match_score DOUBLE PRECISION,
+top1_minus_top2 DOUBLE PRECISION,
+confidence_level VARCHAR(10), 
+manual_review_required BOOLEAN,
+PRIMARY KEY (lcpinspireid)
+);
+
+
+CREATE TABLE IF NOT EXISTS gold.dim_year 
+(
+    year SMALLINT PRIMARY KEY,
+    decade SMALLINT,
+);
+
+CREATE TABLE IF NOT EXISTS  gold.dim_pollutant
+(pollutant_id SMALLINT,
+pollutant VARCHAR(150), 
+PRIMARY KEY (pollutant_id))
+
+CREATE TABLE IF NOT EXISTS gold.dim_sector
+(
+    eprtr_sectorcode INT PRIMARY KEY, 
+    eprtr_sectorname VARCHAR(150), 
+    
+)
+
+CREATE TABLE IF NOT EXISTS gold.dim_site
+(
+    site_id SMALLINT PRIMARY KEY, 
+    facilityinspireid TEXT, 
+    facilityName VARCHAR(150), 
+    city VARCHAR(120), 
+    longitude DOUBLE PRECISION, 
+    latitude DOUBLE PRECISION, 
+
+)
+
+CREATE TABLE IF NOT EXISTS gold.fact_emissions_air
+( year SMALLINT REFERENCES gold.dim_year (year), 
+fk_eprtr_sectorcode INT REFERENCES gold.dim_sector (eprtr_sectorcode), 
+fk_site_id SMALLINT REFERENCES gold.dim_site (site_id),
+fk_pollutant_id SMALLINT REFERENCES gold.dim_pollutant (pollutant_id),
+emitted_pollutant_kg DOUBLE PRECISION, 
+PRIMARY KEY (year, fk_site_id, fk_pollutant_id)
+)
