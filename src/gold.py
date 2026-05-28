97314bc03494a9637df011aab91fc05b61c76a08
olivierpi974 <olivier.picaud@live.fr>
2026-05-28 17:22:41 +0200

    feat(gold): Add mapping id in fact table

--- src/gold.py
+++ src/gold.py
@@ -6,7 +6,7 @@ dim_pollutant_col=['pollutant']
 dim_sector_col= ['eprtr_sectorcode', 'eprtr_sectorname']
 dim_site_col= ['facilityinspireid',	'facilityname',	'city'	,'longitude',	'latitude']
 fact_emission_col=['reportingyear', 'eprtr_sectorcode', 'fk_site_id','fk_pollutant_id',
-       'emitted_pollutant_kg']
+       'emitted_pollutant_tons']
 
 def dim_date():
     """this function returns a dataframe containing the dates
@@ -27,8 +27,8 @@ def dim_pollutant(df):
     
     df=df[dim_pollutant_col].drop_duplicates().reset_index(drop=True)
     df['pollutant_id']=df.index +1
-    
-    return df
+    mapping_id= df.set_index('pollutant')['pollutant_id'].to_dict()
+    return df,mapping_id 
  
 def dim_sector(df):
     
@@ -41,14 +41,17 @@ def dim_sector(df):
 def dim_site(df):
     df=df[dim_site_col].copy()
     df['site_id']=df.index + 1
+    mapping_key=df.setindex('facilityinspireid')['side_id'].to_dict()
+    return df,mapping_key
 
-    return df
-
-def fact_emission_air(df):
+def fact_emission_air(df,mapping_site_id, mapping_pollutant_id):
     
     df.rename(columns={'reportingyear':'year'},inplace=True)
         
     df['emitted_pollutant_kg']=df['emitted_pollutant_kg']//1000
     df.rename(columns={'emitted_pollutant_kg':'emitted_pollutant_tons'}, inplace=True)
-            
+    df['fk_site_id']= df['facilityInspireid'].map(mapping_site_id)
+    df['fk_pollutant_id']=df['pollutant'].map(mapping_pollutant_id)     
+    df=df[fact_emission_col]
+    
     return df 
